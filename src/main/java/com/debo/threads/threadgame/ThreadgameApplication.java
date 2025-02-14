e3552fcafb05b7524f44f207113d8bafaec4efed
Deborpita Ghosh <deborpitaghosh57@gmail.com>
2025-02-14 12:42:04 +0530

    initial commit

---
+++ src/main/java/com/debo/threads/threadgame/ThreadgameApplication.java
@@ -0,0 +1,13 @@
+package com.debo.threads.threadgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debo/threads/threadgame/ThreadgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debo.threads.threadgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
